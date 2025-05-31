bf3ca530c651b48962148bd2b0401dfc53482fc3
Shikhar singh <shikharswat64@gmail.com>
2025-05-31 19:29:26 +0530

    fix: backend url comes from .env

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8001',
+  baseURL: import.meta.env.VITE_BACKEND_URL,
 });
 
 export { axiosInstance };
